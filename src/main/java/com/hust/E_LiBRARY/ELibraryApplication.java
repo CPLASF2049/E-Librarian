e3cf7b82bd36192e31f6de05a560f93367a7384e
HUSTERandENDER <15272984770@163.com>
2024-09-19 17:33:40 +0800

    hhh

---
+++ src/main/java/com/hust/E_LiBRARY/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package com.hust.E_LiBRARY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/E_LiBRARY/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.E_LiBRARY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
